ed4849c391d007c08cacc5871d9d3f02fc1ede98
wwj718 <wuwenjie718@gmail.com>
2020-12-19 14:38:37 +0800

    添加友好的 linda client api

--- codelab_adapter_base.js
+++ codelab_adapter_base.js
@@ -1,5 +1,5 @@
-const io = require("socket.io-client");
-const RateLimiter = require("../../util/rateLimiter.js");
+const io = require("socket.io-client"); // yarn add socket.io-client@2.3.1
+const RateLimiter = require("./rateLimiter.js"); // 独立放一份到这里
 
 class AdapterBaseClient {
     // fork for eim client
@@ -33,6 +33,11 @@ class AdapterBaseClient {
         this.GUI_TOPIC = "gui/operate";
         this.NODES_STATUS_TRIGGER_TOPIC = "core/nodes/status/trigger";
         this.SCRATCH_TOPIC = "scratch/extensions/command";
+
+        // Linda
+        this.LINDA_SERVER = "linda/server"; //target
+        this.LINDA_CLIENT = "linda/client";
+
         this.plugin_topic_map = {
             node: this.NODES_OPERATE_TOPIC,
             extension: EXTS_OPERATE_TOPIC,
@@ -113,6 +118,7 @@ class AdapterBaseClient {
                     // console.debug("NODES_STATUS_TOPIC messsage end");
                     break;
                 }
+
                 // https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Statements/switch
                 // 如果没有break，则自动运行 已匹配到case的下一个 case
                 case NODE_STATU_CHANGE_TOPIC: {
@@ -189,7 +195,28 @@ class AdapterBaseClient {
                         if (this._promiseResolves[message_id]){
                             this._promiseResolves[message_id](content);
                             delete this._promiseResolves[message_id];
-                            console.log({message_id:this._promiseResolves[message_id]});
+                            // console.log({message_id:this._promiseResolves[message_id]});
+                        }
+                    }
+                    break;
+                }
+
+                case this.LINDA_CLIENT: {
+                    const tuple = msg.message.payload.tuple;
+                    console.log(
+                        `LINDA_CLIENT message->`,
+                        tuple
+                    );
+                    // 复用EIM位置参数
+                    if (typeof onAdapterPluginMessage === "function") {
+                        onAdapterPluginMessage(msg);
+                    }
+                    // todo 和 EIM 走同个逻辑管道
+                    if (typeof message_id !== "undefined") {
+                        if (this._promiseResolves[message_id]){
+                            this._promiseResolves[message_id](tuple);
+                            delete this._promiseResolves[message_id];
+                            // console.log({message_id:this._promiseResolves[message_id]});
                         }
                     }
                     break;
@@ -295,8 +322,9 @@ class AdapterBaseClient {
             setTimeout(() => {
                 if (this._promiseResolves[messageID]){
                     console.error(`timeout(${timeout/1000}s)`)
-                    resolve(`timeout(${timeout/1000}s)`);
+                    resolve(`timeout(${timeout/1000}s)`); // reject 积木会中止吗？
                     // todo 通知, 积木名字
+                    // todo: https://github.com/LLK/scratch-vm/blob/acc2e6dba2e5a32668f0b26f0b2c4dfdecbe1023/src/util/jsonrpc.js#L91
                     this.runtime.emit('PUSH_NOTIFICATION', {content: `timeout(${timeout/1000}s)`, type: 'error'})
                 }
                 
@@ -350,7 +378,9 @@ class AdapterBaseClient {
 
     emit_with_messageid(node_id, content, timeout=5000) {
         // todo 添加积木信息，抛出错误
-        if (!this.check_limiter()) return Promise.resolve();
+        if (!this.check_limiter()) return Promise.resolve('rate limit');
+        // socket connected?
+        if (!this.connected) return Promise.resolve('not connected');
         const messageID = this.get_uuid();
         const payload = {};
         payload.node_id = node_id;
@@ -364,6 +394,59 @@ class AdapterBaseClient {
             topic: this.SCRATCH_TOPIC,
         });
         return this.get_reply_message(messageID, timeout);
+    }
+
+    send_to_linda(node_id, payload, timeout=5000) {
+        // send to linda server and wait
+        // 最好是只查询的，不要是有副作用的，永远，不要超时，或者超时，就发送一个目标tuple，抵消原来的请求
+        // todo 添加积木信息，抛出错误
+        // todo: 目前也走 EIM
+        if (!this.check_limiter()) return Promise.resolve('rate limit');
+        // socket connected?
+        if (!this.connected) return Promise.resolve('not connected');
+        
+        // const payload = {};
+        payload.node_id = node_id;
+        // payload.tuple = linda_tuple; //tuple/list
+        const messageID = this.get_uuid();
+        payload.message_id = messageID;
+        if (this.debug_mode){
+            payload.timestamp = new Date().getTime();
+        }
+        this.socket.emit("actuator", {
+            payload: payload,
+            topic: this.LINDA_SERVER,
+        });
+        return messageID;
+        // return this.get_reply_message(messageID, timeout); //todo timeout
+    }
+
+    send_to_linda_and_wait(node_id, payload, timeout=5000) {
+        //返回promise
+        // 返回 promise reject？
+        if (!(Array.isArray(payload["tuple"]) && payload["tuple"].length >0)){return Promise.reject('input error')}
+        let messageID = this.send_to_linda(node_id, payload, timeout);
+        return this.get_reply_message(messageID, timeout); //todo timeout
+    }
+
+    _linda_operate(operate, tuple){
+        let node_id = "linda/js/client";
+        let payload = {};
+        payload["operate"] = operate;
+        payload["tuple"] = tuple;
+        return this.send_to_linda_and_wait(node_id, payload)
+    }
+
+    linda_out(tuple){
+        return this._linda_operate("out", tuple);
+    }
+
+    linda_in(tuple){
+        return this._linda_operate("in", tuple);
+    }
+
+    linda_dump(tuple){
+        return this._linda_operate("dump", tuple);
     }
 
     emit_with_messageid_for_control(node_id, content, node_name, pluginType) {
